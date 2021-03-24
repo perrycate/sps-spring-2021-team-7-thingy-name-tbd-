3dd8d721b35c5248a94aaf92863b6339bfd95226
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-03-24 00:14:59 +0000

    Update frontend and servlt data types

--- App/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ App/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -9,52 +9,78 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
     // switch to true to log data before validating 
-    private static final boolean DEBUGGING = false;
+    private static final boolean DEBUGGING = true;
 
     private void serverLog(
-        String name, String age, String city, String state, String numberOfFamilyMemebers
+        String name, String gender, Integer age, Boolean medicalAssistance, Integer numberOfFamilyMembers, 
+        Boolean disabilities
     ) {
         System.out.println("------------------------");
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
-        System.out.println("State " + state);
-        System.out.println("# Family members: " + numberOfFamilyMemebers);
+        System.out.println("Gender: " + gender);
+        System.out.println("# Family members: " + numberOfFamilyMembers);
+        System.out.println("Disabilities: " + disabilities);
+        System.out.println("Medical Assistance: " + medicalAssistance);
         System.out.println("------------------------");
     }
 
-    private void checkIfValid(
-        String name, String age, String city, String state, String numberOfFamilyMemebers
+    private boolean checkIfValid(
+        String name, String gender, Integer age, Boolean medicalAssistance, Integer numberOfFamilyMembers, 
+        Boolean disabilities
     ) {
         if (name == null || name.length() < 2) {
             System.out.println("Name is too short");
+            return false;
         }
-        if (age == null || age.length() > 3) {
-            System.out.println("Age out of range");
+        if (gender == null) {
+            System.out.println("Gender not selected");
+            return false;
         }
-        if (city == null || city.length() < 2) {
-            System.out.print("City name too small");
+        if (age == null || age < 1 || age > 150) {
+            System.out.println("Age is out of range");
+            return false;
         }
-        if (numberOfFamilyMemebers == null || numberOfFamilyMemebers.length() < 1) {
-            System.out.println("Family is too small");
+        if (medicalAssistance == null) {
+            System.out.println("Medical Assistance not selected");
+            return false;
         }
+        if (numberOfFamilyMembers == null || numberOfFamilyMembers > 25 || 
+            numberOfFamilyMembers < 0) {
+            System.out.println("NFM is out of range");  
+            return false;
+        } 
+        if (disabilities == null) {
+            System.out.println("Disabilities not selected");
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        boolean dataIsValid = true;
 
-        // Get the value entered in the form.
+        // Get the value entered in the form and checks if non string data are valid
         String name = request.getParameter("name");
-        String age = request.getParameter("age");
-        String city = request.getParameter("city");
-        String state = request.getParameter("state");
-        String numberOfFamilyMemebers = request.getParameter("numberOfFamilyMemebers");
-
+        String gender = request.getParameter("gender");
+        Integer age = Integer.valueOf(request.getParameter("age"));
+        Boolean medicalAssistance = Boolean.valueOf(request.getParameter("medicalAssistance"));
+        Integer numberOfFamilyMembers = Integer.valueOf(request.getParameter("numberOfFamilyMembers"));
+        Boolean disabilities = Boolean.valueOf(request.getParameter("disabilities"));
 
         if (DEBUGGING) 
-            serverLog(name, age, city, state, numberOfFamilyMemebers);
+            serverLog(name, gender, age, medicalAssistance, numberOfFamilyMembers, disabilities);
         
-        checkIfValid(name, age, city, state, numberOfFamilyMemebers);
+        dataIsValid = checkIfValid(name, gender, age, medicalAssistance, 
+            numberOfFamilyMembers, disabilities);
 
-        response.sendRedirect("/shelters.html");
+        // this is where the User object would be instantiated and the String types converted
+
+        if (dataIsValid)
+            response.sendRedirect("/shelters.html");
+        else
+            response.sendRedirect("/form.html");
     }
 }
