feb4bc0183b43fc97fdb5d628b61152f2f5f2faa
Jacinda <jacindasoto22@gmail.com>
2021-03-26 20:47:57 +0000

    Add ageRange variables/methods, create ShelterDriver class

---
+++ App/src/main/java/ShelterDriver.java
@@ -0,0 +1,38 @@
+public class ShelterDriver{
+    public static void main(String[] args){
+        Shelter nameOne = new Shelter();
+        nameOne.setName("Youth Emerging Stronger: Runaway & Homeless Minor Youth Program");
+        nameOne.setLocation("1754 Taft Avenue, Los Angeles , CA 90028");
+        nameOne.setPhoneNum(0);
+        nameOne.setDescription();
+        nameOne.setAgeRequirement(12);
+
+        Shelter nameTwo = new Shelter();
+        nameTwo.setName("Youth Emerging Stronger: Transitional Living Program");
+        nameTwo.setLocation("1754 Taft Avenue, Los Angeles , CA 90028");
+        nameTwo.setPhoneNum();
+        nameTwo.setDescription();
+        nameTwo.setAge();
+
+        Shelter nameThree = new Shelter();
+        nameThree.setName();
+        nameThree.setLocation();
+        nameThree.setPhoneNum();
+        nameThree.setDescription();
+        nameThree.setAge();
+
+        Shelter nameFour = new Shelter();
+        nameFour.setName();
+        nameFour.setLocation();
+        nameFour.setPhoneNum();
+        nameFour.setDescription();
+        nameFour.setAge();
+
+        Shelter nameFive = new Shelter();
+        nameFive.setName();
+        nameFive.setLocation();
+        nameFive.setPhoneNum();
+        nameFive.setDescription();
+        nameFive.setAge();
+    }
+}
