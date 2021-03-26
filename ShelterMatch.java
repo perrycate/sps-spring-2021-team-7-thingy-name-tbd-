8c70467182251a5451fe0c85cca169ae94eacb25
Jacinda <jacindasoto22@gmail.com>
2021-03-26 01:00:03 +0000

    Update isEligible method

--- ShelterMatch.java
+++ ShelterMatch.java
@@ -2,25 +2,28 @@ import java.util.ArrayList;
 
 public class ShelterMatch extends Shelter{
     private ArrayList<Shelter> list;
-    private int score;
 
 
-    public ShelterMatch(Shelter shelt, int score){
-         
+    public ShelterMatch(){
+    	list = new ArrayList<Shelter>();
     }
 
-    public addShelter(Shelter s){
-        ShelterMatch shelt = new ShelterMatch(s, score);
-        list.add(shelt);
+    public void addShelter(Shelter s){
+        list.add(s);
     }
 
-    //returns an int array representing the top 3 shelters matched with the user
-    public int[] isElgibile(User person){
-        int[] top3 = new int[3];
+    //returns an array of the top 3 shelters matched with the user
+    public Shelter[] isEligible(User person){
+        Shelter[] top3 = new Shelter[3];
         //comparing each shelter age requirement with the user
         for(int i = 0; i < list.size(); i++){
-            if(me.getAge() < list.get(i).getAgeRequirement()){
-                list.get(i).setScore(this.score++);
+            if(person.getAge() >= list.get(i).getAgeRequirement()){
+                list.get(i).addScore();
+            }
+            //comparing shelters among each other to get the top 3
+            for(int j = 0; j < top3.length; j++){
+            	if(list.get(i).getScore() > top3[j].getScore())
+            		top3[j] = list.get(i);
             }
         }
         return top3;
