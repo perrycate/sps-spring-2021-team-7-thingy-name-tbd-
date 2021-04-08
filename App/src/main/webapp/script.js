9ba4eeaf559ae1cff99cec797ddc6c5b9e0fe6cf
Cameron <cpaytoncodes@gmail.com>
2021-04-08 00:28:31 +0000

    Add faq answer functions

---
+++ App/src/main/webapp/script.js
@@ -0,0 +1,20 @@
+function state(){
+    const state = 'Just califonia for now.';
+    const stateContainer = document.getElementById('state');
+    stateContainer.innerText = state;
+}
+function adult(){
+    const adult = 'No all ages are able to fill this form out.';
+    const adultContainer = document.getElementById('adult');
+    adultContainer.innertext = adult;
+}
+function house(){
+    const house = 'Nope! Everyone is free to fill out the form if need be!';
+    const houseContainer = document.getElementById('housing');
+    houseContainer.innerText = house;
+}
+function shelter(){
+    const shel = 'Yes! As long as you are able to find a shelter that fits you perfectly!';
+    const shelContainer = document.getElementById('shelter');
+    shelContainer.innerText = shel;
+}
