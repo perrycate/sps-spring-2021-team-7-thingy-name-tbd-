cde92a305954b94873061047c9ffdc57c60a9736
Diego Rodrigues <di.rodrigues003@gmail.com>
2021-04-15 01:46:51 +0000

    Add google maps and geocoder API

--- App/src/main/webapp/shelters.js
+++ App/src/main/webapp/shelters.js
@@ -1,17 +1,41 @@
-// Converts URL parameters to JSON
+// Converts URL parameters to JSON.
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const shelters = JSON.parse(urlParams.get('shelters'));
 
 const shelterList = document.querySelector('#main__shelter-list');
 
+// inits google map.
+const map = new google.maps.Map(document.getElementById("main__shelter-map"), {
+  center: { lat: 34.0522, lng: -118.2437 },
+  zoom: 10,
+});
+
+// used for translating street address to lat & lng.
+const geocoder = new google.maps.Geocoder();
+
+// translates regular street addresses into lat & lng.
+function geocodeAddress(geocoder, resultsMap, address) {
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === "OK") {
+      resultsMap.setCenter(results[0].geometry.location);
+      new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location,
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
 for (let shelter of shelters) {
-    // creates a division
+    // creates a division.
     let division = document.createElement('div');
     division.setAttribute('class', 'division');
     shelterList.appendChild(division);
 
-    // creates shelter element
+    // creates shelter element.
     let shelterContainer = document.createElement('div');
     shelterContainer.setAttribute('class', 'shelter-list__shelter');
 
@@ -31,11 +55,14 @@ for (let shelter of shelters) {
     location.setAttribute('class', 'shelter-list__shelter__location');
     location.innerHTML = shelter.location;
 
+    // creates market on map.
+    geocodeAddress(geocoder, map, shelter.location);
+
     shelterContainer.appendChild(name);
     shelterContainer.appendChild(description);
     shelterContainer.appendChild(number);
     shelterContainer.appendChild(location);
 
-    // places shelter into DOM
+    // places shelter into DOM.
     shelterList.appendChild(shelterContainer);
 }
